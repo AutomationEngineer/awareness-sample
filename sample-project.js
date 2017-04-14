af0e70611d7f7edc09697bb61c55a227d28efc79
AutomationEngineer <kulikovv@list.ru>
2017-04-14 14:44:19 +0500

    Move to new project structure

--- sample-project.js
+++ sample-project.js
@@ -1,15 +1,21 @@
-require('awareness');
+var a = require('awareness');
+var fs = require("fs");
 
-Neuron.setDB({host:'192.168.0.125'});
+a.Neuron.setDB({host:'192.168.0.125'});
 
-var node = new Neuron({
+a.Neuron.projectPath = __dirname;
+
+var users = JSON.parse(fs.readFileSync(a.Neuron.projectPath + '/users.json', "utf8"));
+
+
+var node = new a.Neuron({
     name: "Узел редуцирования газа", children: {
-        valve1: new Valve({name: "Кран1"}),
-        csd: new CSD({name: "Сигнализация"})
+        valve1: new a.Valve({name: "Кран1"}),
+        csd: new a.CSD({name: "Сигнализация"})
     }
 });
 
 
-Neuron.afterInit = function(){
-    var web = new Web({secure: false, port: 3000, grafana:'http://192.168.0.125:3001'}, node);
+a.Neuron.afterInit = function(){
+    var web = new a.Web({secure: false, port: 3000, grafana:'http://192.168.0.125:3001', users: users}, node);
 };
